f50d9d1c334e1ebf0d85f7b2432db5fc04892a8f
Ahmed <shiekhahmed135@gmail.com>
2021-05-23 20:15:48 +0500

    login user

--- Bank Management System/src/Home/Login.java
+++ Bank Management System/src/Home/Login.java
@@ -1,7 +1,16 @@
 package Home;
 
+import java.util.Scanner;
+
 public class Login {
     public static void main(String[] args) {
         System.out.println("Login Here!");
+        System.out.print("Username: ");
+        Scanner sc = new Scanner(System.in);
+        String username = sc.nextLine();
+        System.out.print("Password: ");
+        String password = sc.nextLine();
+
+        System.out.println("Welcome '" + username + "'");
     }
 }
